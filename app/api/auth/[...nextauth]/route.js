3abca7e6b57bdb3d9580e1421d08b4d0c317d871
jugdernamjil nymdorj <125348532+thejvgee@users.noreply.github.com>
2025-09-28 21:55:12 +0800

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,8 +11,9 @@ const handler = NextAuth({
     GoogleProvider({
       clientId : process.env.GOOGLE_ID,
       clientSecret : process.env.GOOGLE_SECRET
-    })
-  ]
-})
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
 export {handler as GET , handler as POST}
